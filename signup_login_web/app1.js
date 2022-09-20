849b6333f872a39f7836427260697e8c71aa7672
muzamilsikander <102538533+Muzamil-Sikander@users.noreply.github.com>
2022-09-20 15:20:05 +0500

    Add files via upload

---
+++ signup_login_web/app1.js
@@ -0,0 +1,37 @@
+function viewProfile() {
+    var mainMovieDiv = document.getElementById("container").style.display = "none";
+    var userProfile = document.getElementById("container2").style.display = "block";
+}
+
+// var names = document.getElementsByClassName("username").value;
+// var pass = document.getElementsByClassName("password").value;
+// var conPass = document.getElementsByClassName("confirmPassword").value;
+// var address1 = document.getElementsByClassName("address").value;
+// var dob = document.getElementsByClassName("dob").value;
+// var gender1 = document.getElementsByClassName("gender").value;
+// var profPic = document.getElementsByClassName("profilePic").value;
+// var num = document.getElementsByClassName("number").value;
+// var postCode = document.getElementsByClassName("postalCode").value;
+
+//   var username = document.getElementById("username").value;
+//   var email = document.getElementById("email").value;
+//   var password = document.getElementById("password").value;
+//   var confirmPassword = document.getElementById("confirmPassword").value;
+//   var address = document.getElementById("address").value;
+//   var dob = document.getElementById("dob").value;
+//   var arrayGender = document.getElementsByName("gender");
+//   var selectedGender;
+//   var profilePic = document.getElementById("picChoose").value;
+//   var number = document.getElementById("number").value;
+//   var postalCode = document.getElementById("postalCode").value;
+
+function movieSelect() {
+    var mainMovieDiv = document.getElementById("container").style.display = "block";
+    var userProfile = document.getElementById("container2").style.display = "none";
+}
+function againSignIn() {
+    var mainMovieDiv = document.getElementById("container").style.display = "none";
+    var userProfile = document.getElementById("container2").style.display = "none";
+    // var signin = document.getElementById("signinMain").style.display = "block"; 
+}
+
---
+++ signup_login_web/movie4/app5.js
@@ -0,0 +1,152 @@
+function movieOneShow() {
+    var cont = document.getElementById("container").style.display = "block";
+    var cont2 = document.getElementById("container2").style.display = "none";
+}
+function bookSeat() {
+    var cont2 = document.getElementById("container2").style.display = "block";
+    var cont = document.getElementById("container").style.display = "none";
+}
+function whenSelected1() {
+    var a1 = document.getElementById("rBtns1").style.backgroundColor = "yellow";
+}
+function whenSelected2() {
+    var a2 = document.getElementById("rBtns2").style.backgroundColor = "yellow";
+}
+function whenSelected3() {
+    var a3 = document.getElementById("rBtns3").style.backgroundColor = "yellow";
+}
+function whenSelected4() {
+    var a4 = document.getElementById("rBtns4").style.backgroundColor = "yellow";
+}
+function whenSelected5() {
+    var a5 = document.getElementById("rBtns5").style.backgroundColor = "yellow";
+}
+function whenSelected6() {
+    var a6 = document.getElementById("rBtns6").style.backgroundColor = "yellow";
+}
+function whenSelected7() {
+    var a7 = document.getElementById("rBtns7").style.backgroundColor = "yellow";
+}
+function whenSelected8() {
+    var a8 = document.getElementById("rBtns8").style.backgroundColor = "yellow";
+}
+function whenSelected9() {
+    var a9 = document.getElementById("rBtns9").style.backgroundColor = "yellow";
+}
+function whenSelected10() {
+    var a10 = document.getElementById("rBtns10").style.backgroundColor = "yellow";
+}
+function whenSelected11() {
+    var a11 = document.getElementById("rBtns11").style.backgroundColor = "yellow";
+}
+function whenSelected12() {
+    var a12 = document.getElementById("rBtns12").style.backgroundColor = "yellow";
+}
+function whenSelected13() {
+    var a13 = document.getElementById("rBtns13").style.backgroundColor = "yellow";
+}
+function whenSelected14() {
+    var a14 = document.getElementById("rBtns14").style.backgroundColor = "yellow";
+}
+function whenSelected15() {
+    var a15 = document.getElementById("rBtns15").style.backgroundColor = "yellow";
+}
+function whenSelected16() {
+    var a16 = document.getElementById("rBtns16").style.backgroundColor = "yellow";
+}
+function whenSelected17() {
+    var a17 = document.getElementById("rBtns17").style.backgroundColor = "yellow";
+}
+function whenSelected18() {
+    var a18 = document.getElementById("rBtns18").style.backgroundColor = "yellow";
+}
+function whenSelected19() {
+    var a19 = document.getElementById("rBtns19").style.backgroundColor = "yellow";
+}
+function whenSelected20() {
+    var a20 = document.getElementById("rBtns20").style.backgroundColor = "yellow";
+}
+function whenSelected21() {
+    var a21 = document.getElementById("rBtns21").style.backgroundColor = "yellow";
+}
+function whenSelected22() {
+    var a22 = document.getElementById("rBtns22").style.backgroundColor = "yellow";
+}
+function whenSelected23() {
+    var a23 = document.getElementById("rBtns23").style.backgroundColor = "yellow";
+}
+function whenSelected24() {
+    var a24 = document.getElementById("rBtns24").style.backgroundColor = "yellow";
+}
+function whenSelected25() {
+    var a25 = document.getElementById("rBtns25").style.backgroundColor = "yellow";
+}
+function whenSelected26() {
+    var a26 = document.getElementById("rBtns26").style.backgroundColor = "yellow";
+}
+function whenSelected27() {
+    var a27 = document.getElementById("rBtns27").style.backgroundColor = "yellow";
+}
+function whenSelected28() {
+    var a28 = document.getElementById("rBtns28").style.backgroundColor = "yellow";
+}
+function whenSelected29() {
+    var a29 = document.getElementById("rBtns29").style.backgroundColor = "yellow";
+}
+function whenSelected30() {
+    var a30 = document.getElementById("rBtns30").style.backgroundColor = "yellow";
+}
+function whenSelected31() {
+    var a31 = document.getElementById("rBtns31").style.backgroundColor = "yellow";
+}
+function whenSelected32() {
+    var a32 = document.getElementById("rBtns32").style.backgroundColor = "yellow";
+}
+function whenSelected33() {
+    var a33 = document.getElementById("rBtns33").style.backgroundColor = "yellow";
+}
+function whenSelected34() {
+    var a34 = document.getElementById("rBtns34").style.backgroundColor = "yellow";
+}
+function whenSelected35() {
+    var a35 = document.getElementById("rBtns35").style.backgroundColor = "yellow";
+}
+function whenSelected36() {
+    var a36 = document.getElementById("rBtns36").style.backgroundColor = "yellow";
+}
+function whenSelected37() {
+    var a37 = document.getElementById("rBtns37").style.backgroundColor = "yellow";
+}
+function whenSelected38() {
+    var a38 = document.getElementById("rBtns38").style.backgroundColor = "yellow";
+}
+function whenSelected39() {
+    var a39 = document.getElementById("rBtns39").style.backgroundColor = "yellow";
+}
+function whenSelected40() {
+    var a40 = document.getElementById("rBtns40").style.backgroundColor = "yellow";
+}
+function whenSelected41() {
+    var a41 = document.getElementById("rBtns41").style.backgroundColor = "yellow";
+}
+function whenSelected42() {
+    var a42 = document.getElementById("rBtns42").style.backgroundColor = "yellow";
+}
+function whenSelected43() {
+    var a43 = document.getElementById("rBtns43").style.backgroundColor = "yellow";
+}
+function whenSelected44() {
+    var a44 = document.getElementById("rBtns44").style.backgroundColor = "yellow";
+}
+function whenSelected45() {
+    var a45 = document.getElementById("rBtns45").style.backgroundColor = "yellow";
+}
+function whenSelected46() {
+    var a46 = document.getElementById("rBtns46").style.backgroundColor = "yellow";
+}
+function whenSelected47() {
+    var a47 = document.getElementById("rBtns47").style.backgroundColor = "yellow";
+}
+function whenSelected48() {
+    var a48 = document.getElementById("rBtns48").style.backgroundColor = "yellow";
+}
